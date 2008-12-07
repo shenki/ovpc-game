a03d8c890beb9fa0c778c1a1241d7036465d14db
Joel Stanley <joel.stanley@adelaide.edu.au>
2008-12-07 13:26:23 +1100

    implimented controling of child movement, one raptor

--- ovpc.py
+++ ovpc.py
@@ -2,31 +2,41 @@
 
 import pyglet
 from pyglet.window import key
+from random import random
+import math
 
-window = pyglet.window.Window(caption='OVPC')
+window = pyglet.window.Window(800, 400, caption='OVPC')
+keys = pyglet.window.key.KeyStateHandler()
+window.push_handlers(keys)
 
 batch = pyglet.graphics.Batch()
 
 raptor = pyglet.sprite.Sprite(pyglet.resource.image('raptor.png'), batch=batch)
 child = pyglet.sprite.Sprite(pyglet.resource.image('child.png'), batch=batch)
 
-@window.event
-def on_key_press(symbol, modifiers):
-	if symbol == key.UP:
-		pass
-	elif symbol == key.DOWN:
-		pass
-	elif symbol == key.LEFT:
-		pass
-	elif symbol == key.RIGHT:
-		pass
-	elif symbol == key.ESCAPE:
+def update(dt):
+	if keys[key.ESCAPE]:
 		import sys
 		sys.exit(0)
+
+	if keys[key.UP]:
+		child.y += dt * 100
+	if keys[key.DOWN]:
+		child.y -= dt * 100
+	if keys[key.LEFT]:
+		child.x -= dt * 100
+	if keys[key.RIGHT]:
+		child.x += dt * 100
+
+	raptor.x -= dt * 100
+	raptor.y += dt * random() * 10
 
 @window.event
 def on_draw():
 	window.clear()
 	batch.draw()
 
+raptor.x = window.width
+raptor.y = window.height * random()
+pyglet.clock.schedule(update)
 pyglet.app.run()
