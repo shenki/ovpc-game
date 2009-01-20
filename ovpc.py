747a90f1c2b05f3c36aa94e37bfbb914458b9185
Tim Ansell <mithro@mithis.com>
2009-01-20 21:59:49 +1100

    Really, really crappy collision detection.

--- ovpc.py
+++ ovpc.py
@@ -4,6 +4,7 @@ import pyglet
 from pyglet.window import key
 from random import randrange, random
 import math
+import time
 from pyglet.gl import *
 
 window = pyglet.window.Window(800, 400, caption='OVPC')
@@ -48,12 +49,24 @@ def update(dt):
 	if child.y < 0:
 		child.y = 0
 
-	for raptor in pack:
+	for i, raptor in enumerate(pack):
 		if raptor.x < (0 - raptor.width):
 			reset_enemy(raptor)
 
 		raptor.x -= dt * 150
 		raptor.y += dt * randrange(-100, 100)
+
+		if raptor.x < child.x:
+			continue
+
+		if raptor.y+raptor.height*0.5 < child.y:
+			continue
+
+		if raptor.y-raptor.height*0.5 > child.y+child.height*0.5:
+			continue
+	
+		if (raptor.x-child.x) < (child.width+raptor.width)*0.5-10:
+			print time.time(), "Collision with", i
 
 @window.event
 def on_draw():
