fa682c20c77fe1eed52726542cc12a34ea21bf79
Tim Ansell <mithro@mithis.com>
2009-01-20 23:26:42 +1100

    Add death animation and raptor animation.

--- ovpc.py
+++ ovpc.py
@@ -5,6 +5,7 @@ from pyglet.window import key
 from random import randrange, random
 import math
 import time
+import glob
 from pyglet.gl import *
 
 window = pyglet.window.Window(800, 400, caption='OVPC')
@@ -13,19 +14,33 @@ window.push_handlers(keys)
 
 batch = pyglet.graphics.Batch()
 
+child_death_files = glob.glob("child-death[012]*.png")
+child_death_files.sort()
+child_death = pyglet.image.Animation.from_image_sequence(
+	[pyglet.resource.image(x) for x in child_death_files],
+	0.1, False)
+
+raptor_anim_files = glob.glob("raptor[123].png")
+raptor_anim_files.sort()
+raptor_anim = pyglet.image.Animation.from_image_sequence(
+	[pyglet.resource.image(x) for x in raptor_anim_files],
+	0.1, True)
+
 pack = [
-		pyglet.sprite.Sprite(pyglet.resource.image('raptor.png'), batch=batch),
-		pyglet.sprite.Sprite(pyglet.resource.image('raptor.png'), batch=batch),
-		pyglet.sprite.Sprite(pyglet.resource.image('raptor.png'), batch=batch),
-		pyglet.sprite.Sprite(pyglet.resource.image('raptor.png'), batch=batch),
-		pyglet.sprite.Sprite(pyglet.resource.image('raptor.png'), batch=batch),
-		pyglet.sprite.Sprite(pyglet.resource.image('raptor.png'), batch=batch),
-		pyglet.sprite.Sprite(pyglet.resource.image('raptor.png'), batch=batch),
-		pyglet.sprite.Sprite(pyglet.resource.image('raptor.png'), batch=batch),
-		pyglet.sprite.Sprite(pyglet.resource.image('raptor.png'), batch=batch)]
+		pyglet.sprite.Sprite(raptor_anim, batch=batch),
+		pyglet.sprite.Sprite(raptor_anim, batch=batch),
+		pyglet.sprite.Sprite(raptor_anim, batch=batch),
+		pyglet.sprite.Sprite(raptor_anim, batch=batch),
+		pyglet.sprite.Sprite(raptor_anim, batch=batch),
+		pyglet.sprite.Sprite(raptor_anim, batch=batch),
+		pyglet.sprite.Sprite(raptor_anim, batch=batch),
+		pyglet.sprite.Sprite(raptor_anim, batch=batch),
+		pyglet.sprite.Sprite(raptor_anim, batch=batch)]
 child = pyglet.sprite.Sprite(pyglet.resource.image('child.png'), batch=batch)
 
 def update(dt):
+	global child
+
 	if keys[key.ESCAPE]:
 		import sys
 		sys.exit(0)
@@ -66,7 +81,13 @@ def update(dt):
 			continue
 	
 		if (raptor.x-child.x) < (child.width+raptor.width)*0.5-10:
-			print time.time(), "Collision with", i
+			raptor.x += dt * 150
+
+			if child.image is child_death:
+				continue
+			child.image = child_death
+
+
 
 @window.event
 def on_draw():
